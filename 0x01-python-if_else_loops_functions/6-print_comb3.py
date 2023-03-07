0bcf827bb948b977c6963d8ef55b885fbe6067ea
vinsky001 <ekibet544@gmail.com>
2023-03-07 06:30:54 -0800

    function that checks for lowercase character.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for num in range(10):
-    for j in range(num+1, 10):
-        print("{}, {}".format(num, j), end=", ")
-	else:
-	print("\b\b\n")
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{}, {}".format(i, j), end=", ")
+print("\b\b\n")
