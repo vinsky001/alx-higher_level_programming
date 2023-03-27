16ce215f6b01dbbad233bde006f5e8dc229769f9
vinsky001 <ekibet544@gmail.com>
2023-03-27 09:37:35 -0700

    Exception Error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             total += 1
-        except IndexError:
+        except Exception:
             break
     print("")
     return (total)
