a5f5453553ab60497ae59cd70f60a99b570e7cc6
vinsky001 <ekibet544@gmail.com>
2023-03-27 13:23:46 -0700

    a function that divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
         A new list of length list_length containing all the divisions.
     """
     new_list = []
-    for i in range(0, list_length)
+    for i in range(0, list_length):
     try:
         div = my_list_1[i] / my_list_2[i]
     except TypeError:
