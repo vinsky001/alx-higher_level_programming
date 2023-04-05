6fcc390fa9dca19dfab9744ff3c6d179b1409e07
vinsky001 <ekibet544@gmail.com>
2023-04-04 23:31:03 -0700

    a function that prints a square with the character #.

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """establishing a name  printing function"""
 
+
 def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
