e587a58f2d4c355d59cf442136371fe1aadd4e82
vinsky001 <ekibet544@gmail.com>
2023-04-11 01:50:51 -0700

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""First go for inheritance"""
+
+
+class MyList(list):
+    """access all methods from lists"""
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(list):
+        """prints sorted lists"""
+        print(sorted(list))
