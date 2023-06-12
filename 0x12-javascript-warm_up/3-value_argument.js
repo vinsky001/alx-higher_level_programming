871e793ce314c16ec902a6ac2220d38763746818
vinsky001 <ekibet544@gmail.com>
2023-06-12 04:40:28 -0700

    script that prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const [arg] = process.argv.slice(2) // ignores the last 2  argumentspassed //
+const [arg] = process.argv.slice(2); // ignores the last 2  argumentspassed //
 
 if (arg === undefined) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
   console.log(arg);
 }
