cfd6fe99a1ab10badaa0dae1986267b4372b07ae
vinsky001 <ekibet544@gmail.com>
2023-06-14 02:51:59 -0700

    script that concats 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/noje
+
+const fs = require('fs');
+const path = require('path');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+const data1 = fs.readFileSync(path.join(__dirname, sourceFile1));
+const data2 = fs.readFileSync(path.join(__dirname, sourceFile2));
+
+fs.writeFileSync(path.join(__dirname, destinationFile), data1 + data2);
