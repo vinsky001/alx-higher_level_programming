b284e2dc984d3cf4cb3e7daca39e4b3a9b9d8238
vinsky001 <ekibet544@gmail.com>
2023-06-16 05:48:42 -0700

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 """
  lists all states from the database hbtn_0e_0_usa
 should connect to a MySQL server running on localhost
-""" 
+"""
 import sys
 import MySQLdb
 
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     # Get my SQL connection parameters from the command line #
     username, password, database = sys.argv[1:4]
 
-    #Create connection object and cursor#
-    connection = MySQLdb.connect(host="localhost", user="username", password="password", db="database")
+    # Create connection object and cursor#
+    connection = MySQLdb.connect(user="username", password="password", db="database")
     # Create a cursor object
     cursor = connection.cursor()
 
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     # Fetch the results
     results = cursor.fetchall()
 
-    #print results
+    # print results
     for states in results:
         print(states)
 
-    # Close the cursor and the database connection
+    # Close cursor and the database connection
     cursor.close()
     connection.close()
