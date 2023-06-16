682e87bc4540e43dc7496d55de0d4b6512934858
vinsky001 <ekibet544@gmail.com>
2023-06-16 11:55:58 -0700

    Lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Listsall cities from the database hbtn_0e_4_usa
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ = __"main"__:
+    # Connect to MySQL dbwith provided credentials#
+    connection = MySQLdb.connect(user=[argv1], passwd=[argv2], db=[argv3])
+
+    # Create a cursor object
+    cur = connection.cursor()
+
+    # Execute SQL query that lists cities from corresponding names#
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                    FROM cities JOIN states ON cities.state_id = states.id
+                    ORDER BY cities.id ASC""")
+
+    # Fetch  and print/display cities#
+    [print(city), for city in cur.fetchall()]
