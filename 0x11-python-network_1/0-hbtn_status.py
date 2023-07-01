a4ade12d1c713deecaa03c4d0444dcfdf7a5b6eb
vinsky001 <ekibet544@gmail.com>
2023-07-01 02:01:59 -0700

    Fixing output of the code to display content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,15 +8,16 @@ Python script that:
 - like the following example (tabulation before -)
 - must use a with statement
 """
+
 if __name__ == "__main__":
     from urllib.request import urlopen, Request
 
     url = "https://alx-intranet.hbtn.io/status"
     req = Request(url)
-    with urllib.request.urlopen(req) as res:
+    with urlopen(req) as res:
         content = res.read()
 
     print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf-8')}")
+    print(f"\t- type:", type(content))
+    print(f"\t- content:", (content))
+    print(f"\t- utf8 content:", content.decode('utf-8'))
