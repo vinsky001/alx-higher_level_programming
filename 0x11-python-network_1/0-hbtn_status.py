f6e3b37e64ec682618595840b82c3f7133818d0f
vinsky001 <ekibet544@gmail.com>
2023-07-01 01:15:03 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that:
+- fetches https://alx-intranet.hbtn.io/status
+- must use the package urllib
+- not allowed to import any packages other than urllib
+- The body of the response must be displayed
+- like the following example (tabulation before -)
+- must use a with statement
+"""
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    request = request(url)
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+
+    print("Body response:")
+    print(f"\t- type:", type(content))
+    print(f"\t- content:", (content))
+    print(f"\t- utf8 content:", content.decode('utf-8'))
