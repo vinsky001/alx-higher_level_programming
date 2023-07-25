bdfc414a46f6d1afc0a39a30f6411155709ba1d6
vinsky001 <ekibet544@gmail.com>
2023-07-25 15:51:24 +0000

    Fixed indentation

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,8 +5,7 @@ const fName = process.argv[2];
 const data = process.argv[3];
 
 fs.writeFile(fName, data, 'utf-8', function (err, data) {
-	  if (err) {
-		      console.log(err);
-	  }
+  if (err) {
+    console.log(err);
+  }
 });
-
