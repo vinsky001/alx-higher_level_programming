0a362373661ca080bb068d02268fca07d1d5b95d
vinsky001 <ekibet544@gmail.com>
2023-07-25 21:45:06 +0000

    script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
-
+/**
+ *   compute number of tasks completed by user id
+ *     usage: /6-completed_tasks.js <API URL>
+ *       */
+const myArgs = process.argv.slice(2);
 const request = require('request');
-const url = process.argv[2];
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  }
-
-  const todos = JSON.parse(body);
-  const data = {};
-
-  todos.forEach((todo) => {
-    if (todo.completed && data[todo.userId] === undefined) {
-      data[todo.userId] = 1;
-    } else if (todo.completed) {
-      data[todo.userId] += 1;
+const id_ = [];
+const result = {};
+request(myArgs[0], function (error, response, body) {
+  let i;
+  if (!error) {
+    const json_ = JSON.parse(body);
+    const len = json_.length;
+    for (i = 0; i < len; i++) {
+      if (json_[i].completed) {
+        id_.push(json_[i].userId);
+      }
     }
-  });
-
-  console.log(data);
+    for (let j = 0; j < id_.length; ++j) {
+      if (!result[id_[j]]) { result[id_[j]] = 0; }
+      ++result[id_[j]];
+    }
+  }
+  console.log(result);
 });
